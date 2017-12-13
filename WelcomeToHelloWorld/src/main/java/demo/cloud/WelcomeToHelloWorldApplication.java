cbf4b347b37d7eec186f3fe3c1fb7d52eaa56d91
saisanthosh09 <34261919+saisanthosh09@users.noreply.github.com>
2017-12-13 15:47:48 +0530

    Add files via upload

---
+++ WelcomeToHelloWorld/src/main/java/demo/cloud/WelcomeToHelloWorldApplication.java
@@ -0,0 +1,12 @@
+package demo.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WelcomeToHelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WelcomeToHelloWorldApplication.class, args);
+	}
+}
